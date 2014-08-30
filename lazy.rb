d9157de4a5d00d1161648887aabf6a769cae9b7d
Chen Fisher <chen.fisher@gmail.com>
2014-08-30 18:57:34 +0300

    indentation

--- lazy.rb
+++ lazy.rb
@@ -21,7 +21,7 @@ class Enumerator
   def lazy_select(&block) 
     Enumerator.new do |yielder| 
       self.each do |val| 
-      yielder.yield(val) if block.call(val) 
+	      yielder.yield(val) if block.call(val) 
       end 
     end 
   end 
