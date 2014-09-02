d1aa196569c1245f7faded51ef8e793bc76bb0bd
Chen Fisher <chen.fisher@gmail.com>
2014-09-02 08:24:20 +0300

    higher order functions

--- higher_order_functions.rb
+++ higher_order_functions.rb
@@ -7,7 +7,7 @@ reverse = proc { |x| x.reverse }
 # usage
 names.map &capitalize
 names.map &reverse
-names.map(&reverse).map(&capitalize)
+names.map(&reverse).map(&capitalize) # iterates twice over the array!
 
 # obviously we do not really need capitalize and reverse procs since
 # we can do this with the String's capitalize and reverse methods:
@@ -15,13 +15,13 @@ names.map(&:capitalize).map(&:reverse)
 # but we define the procs for the purpose of demonstration
 
 # compose a function from two functions
-def comp(f1, f2)
+def compose(f1, f2)
 	proc { |*args| f1.call(f2.call(*args)) }
 end
 
 # usage
-reverse_and_capitalize = comp capitalize, reverse
-names.map &reverse_and_capitalize
+reverse_and_capitalize = compose capitalize, reverse
+names.map &reverse_and_capitalize # iterates only once over the array
 
 # a cool hack
 class Proc
